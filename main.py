430c9251244bbb83e73d412f1558dda6bc85fd34
JJPay (Certified Concrete Eater) <hazardrockband1@gmail.com>
2026-03-04 18:13:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -346,8 +346,8 @@ async def verified(ctx, member: discord.Member):
         if u_role in member.roles: await member.remove_roles(u_role)
         await member.add_roles(v_role)
         await member.add_roles(m_role)
-        await ctx.send(f"✅ **{member.display_name}** is now a verified member of Marketpro Lounge!")
-    except Exception as e: await ctx.send(f"❌ Error: {e}")
+        await ctx.send(f"✅ **{member.display_name}** is now a verified member of Marketpro Lounge! They will recieve pings every time a marketing tip is posted, they will have to ask for it to be removed if they do not won't pings.")
+    except Exception as e: await ctx.send(f"❌ Error: {e}"
 
 @bot.command()
 @commands.has_any_role("MODERATOR", "Jr. Assistant", "Assistant", "Jr. MODERATOR")
